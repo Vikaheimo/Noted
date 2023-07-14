5da8ee4c878041346d76c9725f63dc7f005750b1
Vili Ikäheimo <vili.vikaheim@gmail.com>
2023-07-14 21:50:01 +0300

    Handle interactive mode

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,69 @@ use noted::Database;
 fn main() -> Result<(), NoteError> {
     let command = std::env::args().skip(1).collect::<Vec<_>>();
 
+    let db = Database::new();
+
     if command.is_empty() {
-        usage();
+        interactive(&db);
         return Ok(());
     }
 
-    let db = Database::new();
-
-    match command[0].to_lowercase().as_str() {
-        "list" | "l" => list_all(&db),
-        "help" | "h" => help(),
-        "add" | "a" => add(&db, &command)?,
-        _ => usage(),
-    }
-
+    handle_command(command, &db)?;
     Ok(())
+}
+
+fn handle_command(command: Vec<String>, db: &Database) -> Result<(), NoteError> {
+    match command[0].to_lowercase().as_str() {
+        "list" | "l" => list_all(db),
+        "help" | "h" => help(),
+        "add" | "a" => add(db, &command)?,
+        _ => usage(),
+    };
+    Ok(())
+}
+
+fn interactive(db: &Database) {
+    println!("Running in interactive mode, enter empty to exit!");
+    loop {
+        print!("> ");
+        let line: String = text_io::read!("{}\n");;
+        let parsed = parse_string(line);
+        if parsed.is_empty() {
+            return;
+        }
+
+        if let Err(err) = handle_command(parsed, db) {
+            println!("Error: {}", err);
+        }
+    }
+}
+
+fn parse_string(s: String) -> Vec<String> {
+    let mut between_quotes = false;
+    let mut string_buffer = String::new();
+    let mut formatted = vec![];
+    for c in s.chars() {
+        match (c, between_quotes) {
+            ('\'', true) | ('\"', true) => {
+                between_quotes = false;
+                formatted.push(string_buffer);
+                string_buffer = String::new();
+            }
+            ('\'', false) | ('\"', false) => between_quotes = true,
+            (' ', false) => {
+                if string_buffer.is_empty() {
+                    continue;
+                }
+                formatted.push(string_buffer);
+                string_buffer = String::new();
+            }
+            (_, _) => string_buffer.push(c),
+        }
+    }
+    if !string_buffer.is_empty() {
+        formatted.push(string_buffer)
+    }
+    formatted
 }
 
 fn usage() {
@@ -69,4 +117,44 @@ fn help() {
 enum NoteError {
     #[display(fmt = "Missing field: {}", field)]
     MissingField { field: String },
+}
+
+#[cfg(test)]
+mod test {
+    use super::parse_string;
+
+    fn create_string_vector(v: Vec<&str>) -> Vec<String> {
+        v.into_iter().map(|s| s.to_owned()).collect()
+    }
+
+    #[test]
+    fn empty_parsed() {
+        let parse = String::from("");
+        let parsed = parse_string(parse);
+        let model = create_string_vector(vec![]);
+        assert_eq!(parsed, model)
+    }
+    #[test]
+    fn parse_string_without_quotes() {
+        let parse = String::from("no quotations here");
+        let parsed = parse_string(parse);
+        let model = create_string_vector(vec!["no", "quotations", "here"]);
+        assert_eq!(parsed, model)
+    }
+
+    #[test]
+    fn parse_string_with_single_quotes() {
+        let parse = String::from("some 'quotes here'");
+        let parsed = parse_string(parse);
+        let model = create_string_vector(vec!["some", "quotes here"]);
+        assert_eq!(parsed, model)
+    }
+
+    #[test]
+    fn start_and_end_quotes() {
+        let parse = String::from("'please parse ' ' this sentence'");
+        let parsed = parse_string(parse);
+        let model = create_string_vector(vec!["please parse ", " this sentence"]);
+        assert_eq!(parsed, model)
+    }
 }
